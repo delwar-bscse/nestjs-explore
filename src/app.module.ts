7e13e684f44788941a39a54bc887e8f8ae5a66da
Md. Delwar Hossain <delwarbscse@gmail.com>
2026-05-15 10:50:47 +0600

    Connect MongoDB database

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { UserModule } from './user/user.module';
 import { LoggerMiddleware } from './common/middlewares/logger/logger.middleware';
 import { ConfigModule } from '@nestjs/config';
 import { EvService } from './common/config/ev/ev.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [UserModule, ConfigModule.forRoot({ isGlobal: true })],
+  imports: [UserModule, ConfigModule.forRoot({ isGlobal: true }), MongooseModule.forRoot(process.env.DB_URI!)],
   controllers: [AppController],
   providers: [AppService, EvService],
 })
