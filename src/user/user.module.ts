2a4e2879fec8f5e215899de90de5bd2bb2d9b4fd
Md. Delwar Hossain <delwarbscse@gmail.com>
2026-05-15 11:21:22 +0600

    Schema design for user

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserSchema } from './user.schema';
 
 @Module({
-    imports: [],
+    imports: [MongooseModule.forFeature([{ name: "User", schema: UserSchema }])],
     controllers: [UserController],
     providers: [UserService],
 })
